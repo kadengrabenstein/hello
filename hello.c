93ad32c5404fd93f15b41caaedb474b49437b44b
Kaden Grabenstein <kgrabenstein2@huskers.unl.edu>
2020-08-18 20:35:28 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ *
+ * Author: Kaden Grabenstein
+ * 8/18/2020
+ * My simple hello world file for Hack 1
+ *
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ int main(int argc, char **argv) {
+
+     printf("Kaden Grabenstein\n");
+
+     return 0;
+ }
